0860a65dc49ee69733611b97a0874dc50eb93b29
alittleton98 <andrew.littleton1998@gmail.com>
2022-08-31 22:42:25 -0500

    header values established

--- ffmx_pack/include/MultitrackWav.h
+++ ffmx_pack/include/MultitrackWav.h
@@ -13,6 +13,7 @@ A "track" in this case, is a container for one or multiple channels of audio wit
 #define MULTITRACK_WAVE_FOURCC 'MTWV'
 #define RIFF_FOURCC 'RIFF'
 
+
 typedef unsigned long DWORD;
 typedef unsigned char BYTE;
 typedef DWORD FOURCC;
@@ -31,7 +32,7 @@ typedef struct
 	union track_channel_count { int nChannels; }; // number of channels to be accounted for 
 	union track_count { int nTracks; }; // number of "tracks" packed within the file
 	union track_sample_rate { DWORD IntendedSamplesPerSecond; }; // number of samples that are to be read for each track
-	union file_sample_rate { DWORD TotalSamplesPerSecond; }; // number of total samples read per second. Should be track_sample_rate * track_count * track_channel_count, but should NOT exceed 192000 
+	union file_sample_rate { DWORD TotalSamplesPerSecond; }; // number of total samples read per second. Should be track_sample_rate * track_count, but should NOT exceed 192000 
 	union file_data_rate { DWORD TotalBytesPerSecond; }; // total bytes read per second by the device
 	union track_block_alignment { int IntendedBlockSize; }; // block size per track in bytes
 	union file_block_alignment { int TotalBlockSize; }; // block size for all tracks read
@@ -45,11 +46,48 @@ typedef struct
 } mtwv_header;
 
 // Creates file at path with header chunks
-FILE CreateMTWVNative(BYTE format_type, int nChannels, int nTracks, DWORD IntendedSamplesPerSecond, string path)
+FILE CreateMTWVNative(BYTE format_type, int nChannels, int nTracks, DWORD IntendedSamplesPerSecond, int bitDepth, std::string path)
 {
 	FILE mtwvFIle;
 	mtwv_header header;
-	ofstream untitledMTWV;
-	// TODO: Write header chunk information to file calculated from parameters
+	std::ofstream untitledMTWV;
+	
+	// Set non-union calculated variables
+	header.riff_chunk_id = RIFF_FOURCC;
+	header.riff_formatchunk_id = MULTITRACK_WAVE_FOURCC;
+	header.format_chunk_id = 'fmt';
+	header.format_type = format_type;
+	header.data_chunk_id = 'data';
+
+	// Set union calculated variables (denoted with Pascal case)
+	mtwv_header::riff_chunk_size RiffChunkSize;
+	RiffChunkSize.file_size = 0; // Set to 0 as default. Replace after data is ready
+	mtwv_header::format_chunk_size FormatChunkSize;
+	// Following chunk size is NOT equivalent to the regular WAVE format chunk size as there are extra parameters 
+	FormatChunkSize.format_header_size = sizeof(mtwv_header::format_type) + sizeof(mtwv_header::track_channel_count) + sizeof(mtwv_header::track_count) + sizeof(mtwv_header::track_sample_rate) + sizeof(mtwv_header::file_sample_rate) + sizeof(mtwv_header::file_data_rate) + sizeof(mtwv_header::track_block_alignment) + sizeof(mtwv_header::file_block_alignment) + sizeof(mtwv_header::bit_depth); 
+	mtwv_header::track_channel_count TrackChannelCount;
+	TrackChannelCount.nChannels = nChannels;
+	mtwv_header::track_count TrackCount;
+	TrackCount.nTracks = nTracks;
+	mtwv_header::track_sample_rate TrackSampleRate;
+	TrackSampleRate.IntendedSamplesPerSecond = IntendedSamplesPerSecond;
+	mtwv_header::file_sample_rate FileSampleRate;
+	FileSampleRate.TotalSamplesPerSecond = TrackSampleRate.IntendedSamplesPerSecond * TrackCount.nTracks; // this should not exceed 192kHz
+	mtwv_header::file_data_rate FileDataRate;
+	mtwv_header::track_block_alignment TrackBlockAlign;
+	TrackBlockAlign.IntendedBlockSize = (TrackChannelCount.nChannels * bitDepth)/8;
+	mtwv_header::file_block_alignment FileBlockAlign;
+	FileBlockAlign.TotalBlockSize = (TrackChannelCount.nChannels * TrackCount.nTracks * bitDepth) / 8;
+	FileDataRate.TotalBytesPerSecond = (FileSampleRate.TotalSamplesPerSecond * TrackCount.nTracks * TrackChannelCount.nChannels * bitDepth)/8;
+	mtwv_header::bit_depth BitDepth;
+	BitDepth.nBitsPerSample = bitDepth;
+
+	// Assumed to have a proper file name
+	untitledMTWV.open(path, std::ios::out);
+	// Write data in order from the header
+	
+
+
+	return mtwvFIle;
 }
 
